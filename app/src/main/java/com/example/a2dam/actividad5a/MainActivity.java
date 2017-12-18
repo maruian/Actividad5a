5cf3cf978714826579daaf5e84b0becd22a8d1c9
maruian@floridauniversitaria.es <maruian@floridauniversitaria.es>
2017-12-18 20:15:01 +0100

    Initial commit

---
+++ app/src/main/java/com/example/a2dam/actividad5a/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.a2dam.actividad5a;
+
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements Opciones.OnFragmentInteractionListener, AltaUsuario.OnFragmentInteractionListener {
+
+
+    FragmentManager fm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public boolean estaFragmentDinamic() {
+        Fragment f = fm.findFragmentById(R.id.fragment_dinamic);
+        if (f == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/a2dam/actividad5a/model/Usuario.java
@@ -0,0 +1,60 @@
+package com.example.a2dam.actividad5a.model;
+
+/**
+ * Created by 2dam on 18/12/2017.
+ */
+public class Usuario {
+    private String usuario;
+    private String correo;
+    private String nombre;
+    private String apellidos;
+    private String direccion;
+
+    public Usuario(String usuario, String correo, String nombre, String apellidos, String direccion) {
+        this.usuario = usuario;
+        this.correo = correo;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.direccion = direccion;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
